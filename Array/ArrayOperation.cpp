93f391e17b9020e6c3a850d510920dda6e406f70
Abhishek <loginabhi@hotmail.com>
2024-03-02 21:09:53 +0530

    binary search

--- Array/ArrayOperation.cpp
+++ Array/ArrayOperation.cpp
@@ -82,6 +82,54 @@ int LinearSearch(struct Array *arr, int key)
     return -1;
 }
 
+// Binary Search (if-else)
+int BinarySearchLoop(struct Array arr, int key)
+{
+    int low = 0, high, mid;
+    high = arr.length - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (key == arr.A[mid])
+        {
+            return mid;
+        }
+        else if (key < arr.A[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+// Binary Search (Recursive)
+int RBinSearch(int a[], int l, int h, int key)
+{
+    int mid;
+    if (l <= h)
+    {
+        mid = (l + h) / 2;
+        if (key == a[mid])
+        {
+            return mid;
+        }
+        else if (key < a[mid])
+        {
+            return RBinSearch(a, l, mid - 1, key);
+        }
+        else
+        {
+            return RBinSearch(a, mid - 1, h, key);
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
@@ -90,7 +138,9 @@ int main()
     // Append(&arr, 10);
     // Insert(&arr, 0, 100);
     // printf("%d\n", Delete(&arr, 2));
-    cout << "key :" << key << "\nindex :" << LinearSearch(&arr, key);
+    // cout << "key :" << key << "\nindex :" << LinearSearch(&arr, key);
+    printf("index :%d", BinarySearchLoop(arr, 5));
+    printf("index :%d", RBinSearch(arr.A,0,arr.length, 5));
     Display(arr);
     return 0;
 }
