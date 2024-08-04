6a5ec61f15110cd25d9efffc80ed1d222dbea571
Abhishek Sen <105163544+abhishek2273@users.noreply.github.com>
2024-08-04 22:46:48 +0530

    Create polymorphism.cpp

---
+++ #OOPs/polymorphism.cpp
@@ -0,0 +1,11 @@
+/*
+The word polymorphism means having many forms. In simple words, we can define polymorphism as the ability of a message to be displayed in more than one form. A person at the same time can have different characteristics. A man at the same time is a father, a husband, and an employee. So the same person possesses different behavior in different situations. This is called polymorphism. An operation may exhibit different behaviors in different instances. The behavior depends upon the types of data used in the operation. C++ supports operator overloading and function overloading.
+
+    Operator Overloading: The process of making an operator exhibit different behaviors in different instances is known as operator overloading.
+    Function Overloading: Function overloading is using a single function name to perform different types of tasks. Polymorphism is extensively used in implementing inheritance.
+
+ For example, imagine a function draw() that can be used to draw different shapes. If you have classes Circle, Square, and Triangle, each with its own implementation of the draw() method, you can call draw() on an object, and it will execute the correct method based on the object's actual class. This allows for flexible and dynamic code that can work with objects of different types in a uniform way.
+
+*/
+
+//----------------------------------------------------------
