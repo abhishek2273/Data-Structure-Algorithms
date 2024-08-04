23b8958f1638eaf2e8c71a14626b868ee2afe53a
Abhishek Sen <105163544+abhishek2273@users.noreply.github.com>
2024-08-04 21:50:25 +0530

    Create Inheritance.cpp

---
+++ #OOPs/Inheritance.cpp
@@ -0,0 +1,6 @@
+/*
+Inheritance is a principle in object-oriented programming where a new class, known as a child or subclass, is created based on an existing class, known as a parent or superclass. The child class inherits attributes and methods from the parent class, allowing for code reuse and the creation of hierarchical relationships between classes.
+
+In simple terms, inheritance is like a family tree. Just as children inherit characteristics from their parents, a subclass inherits properties and behaviors from its superclass. For example, if you have a class Vehicle with attributes like speed and color and methods like move(), you can create a subclass Car that inherits these attributes and methods. The Car class can also have additional attributes and methods specific to cars, like number_of_doors and honk(). This way, Car gets all the features of Vehicle without having to rewrite the common code.
+*/
+
